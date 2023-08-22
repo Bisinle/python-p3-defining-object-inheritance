e4384a628084e227da153a046a9ece217d5a9d6f
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-22 19:42:10 +0300

    passed all the tests

--- lib/car.py
+++ lib/car.py
@@ -4,3 +4,8 @@ from vehicle import Vehicle
 class Car(Vehicle):
     def go(self):
         return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+car = Car(23, 334455)
+print(car.wheel_number)
+print(car.go())  # prints the one it inherited from the superClass
